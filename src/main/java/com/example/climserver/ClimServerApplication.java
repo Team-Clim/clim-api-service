af0cefcde4d24f593e0f69ffed1805f9a9728391
troy96475 <taesupark0504@gmail.com>
2024-09-27 20:19:13 +0900

    first commit

---
+++ src/main/java/com/example/climserver/ClimServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.climserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/climserver/ClimServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
