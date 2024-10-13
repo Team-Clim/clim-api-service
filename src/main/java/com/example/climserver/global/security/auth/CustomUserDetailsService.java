00cb7ad3203520edf59fb40976832ecd9c183c23
troy96475 <taesupark0504@gmail.com>
2024-10-13 17:54:47 +0900

    feat :: security-auth

---
+++ src/main/java/com/example/climserver/global/security/auth/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.example.climserver.global.security.auth;
+
+import com.example.climserver.domain.user.dao.UserRepository;
+import com.example.climserver.domain.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found"));
+
+        return new CustomUserDetails(user);
+    }
+}
