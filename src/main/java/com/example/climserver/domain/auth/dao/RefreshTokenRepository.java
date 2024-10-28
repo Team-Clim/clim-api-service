c296690fc25ce08d97905eea25dbbe3790d65145
troy96475 <taesupark0504@gmail.com>
2024-10-28 11:21:29 +0900

    refactor :: @Repository 추가

--- src/main/java/com/example/climserver/domain/auth/dao/RefreshTokenRepository.java
+++ src/main/java/com/example/climserver/domain/auth/dao/RefreshTokenRepository.java
@@ -2,9 +2,10 @@ package com.example.climserver.domain.auth.dao;
 
 import com.example.climserver.domain.auth.entity.RefreshToken;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
     Optional<RefreshToken> findByToken(String token);
 }
