ad26c5175e25fb06515cb129db1d5f140f4cb9e8
troy96475 <taesupark0504@gmail.com>
2024-10-28 11:22:04 +0900

    feat :: email 관련 controller

---
+++ src/main/java/com/example/climserver/domain/auth/email/api/EmailController.java
@@ -0,0 +1,24 @@
+package com.example.climserver.domain.auth.email.api;
+
+import com.example.climserver.domain.auth.email.application.EmailService;
+import com.example.climserver.domain.auth.email.application.VerificationCodeService;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class EmailController {
+    private final EmailService emailService;
+    private final VerificationCodeService verificationCodeService;
+
+    @PostMapping("/send-verification-code")
+    public void sendVerificationCode(@RequestParam String email) throws MessagingException {
+        String verificationCode = emailService.sendEmailVerification(email);
+        verificationCodeService.saveVerificationCode(email, verificationCode);
+    }
+}
