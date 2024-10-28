5e36d8fbbe2224d99edc7419c03dff133012f497
troy96475 <taesupark0504@gmail.com>
2024-10-28 11:21:14 +0900

    refactor :: @Repository 추가

--- src/main/java/com/example/climserver/domain/user/dao/UserRepository.java
+++ src/main/java/com/example/climserver/domain/user/dao/UserRepository.java
@@ -2,9 +2,10 @@ package com.example.climserver.domain.user.dao;
 
 import com.example.climserver.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
 
