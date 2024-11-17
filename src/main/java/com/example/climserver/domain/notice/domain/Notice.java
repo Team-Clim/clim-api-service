ad5197ae650df46a43bec24061818adff2d250a5
imhanseong <hans_hanseong@dsm.hs.kr>
2024-11-18 08:53:40 +0900

    feat :: Notice 엔티티 생성

---
+++ src/main/java/com/example/climserver/domain/notice/domain/Notice.java
@@ -0,0 +1,32 @@
+package com.example.climserver.domain.notice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String title;
+    @NotNull
+    @Size(max = 255)
+    private String contents;
+    private String name;
+    private LocalDateTime now;
+}
